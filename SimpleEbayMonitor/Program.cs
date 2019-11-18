3f3bcd56ad2808b97b4c85da4b09081c41d78089
Max Kuzmin <max_kuzmin@outlook.com>
2019-11-19 02:08:05 +0300

    Implement

--- SimpleEbayMonitor/Program.cs
+++ SimpleEbayMonitor/Program.cs
@@ -7,7 +7,7 @@ using System.Net;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace SimpleEbayMonitor
 {
     class Program
     {
@@ -16,21 +16,23 @@ namespace ConsoleApp1
         private static readonly string Pattern = "https://www.ebay.com/itm/[^\"]+";
         private static string PricePattern = "item__price\">[^,]+";
         private const int PricePatternSkip = 13;
-        private const int RubPerUsd = 66;
 
-        //https://www.ebay.com/sch/i.html?_sop=10&rt=nc&LH_BO=1&_nkw=
-        //https://www.ebay.com/sch/i.html?_sop=10&rt=nc&LH_BIN=1&_nkw=
+        // Parameters
+        private const double CurrencyMultiplier = 1d / 67;
+        private const int TooManyItemsCount = 10;
+        private const int DelaySeconds = 5;
 
-        static void Main(string[] args)
+        static void Main()
         {
             Task.Run(Action).Wait();
         }
 
         private static async Task Action()
         {
-            Console.WriteLine("Enter search query (ex. 'macbook pro 2017'):");
+            Console.WriteLine("Enter search query (ex. 'macbook pro'):");
             var query = Console.ReadLine();
-            Console.WriteLine("Enter max price in USD (ex. 1000):");
+
+            Console.WriteLine("Enter max price (ex. 1000):");
             var maxPrice = int.Parse(Console.ReadLine());
 
             var items = new List<string>();
@@ -42,9 +44,9 @@ namespace ConsoleApp1
                 Console.WriteLine($"Initial items loaded: {items.Count}");
             }
 
-            while (true)
+            while (!(Console.KeyAvailable && Console.ReadKey().Key == ConsoleKey.Escape))
             {
-                await Task.Delay(TimeSpan.FromSeconds(5));
+                await Task.Delay(TimeSpan.FromSeconds(DelaySeconds));
 
                 string page;
                 try
@@ -69,10 +71,9 @@ namespace ConsoleApp1
                 var newItems = tempNewItems.Except(items).ToArray();
                 items.AddRange(newItems);
                 File.AppendAllLines(ItemsFile, newItems);
-
                 Console.WriteLine($"New items loaded: {newItems.Length}");
 
-                if (newItems.Length > 10)
+                if (newItems.Length > TooManyItemsCount)
                 {
                     Console.WriteLine("Too many new items. Skipping.");
                     continue;
@@ -80,11 +81,10 @@ namespace ConsoleApp1
 
                 foreach (var newItem in newItems)
                 {
-                    var price = int.Parse(newPrices[newItem]
+                    var price = double.Parse(newPrices[newItem]
                                     .Substring(PricePatternSkip)
-                                    .Replace(" ", "")) / RubPerUsd;
-
-                    Console.WriteLine($"Price: {price}, {newItem}");
+                                    .Replace(" ", "")) * CurrencyMultiplier;
+                    Console.WriteLine($"Price: {(int)price}, {newItem}");
 
                     if (price <= maxPrice)
                     {
